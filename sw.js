7a7b4d0a05f173d5a56712e3882a46d0acb73ae6
ray-pH <rayhan.azizi9@gmail.com>
2025-10-27 04:47:45 +0700

    add icons

--- sw.js
+++ sw.js
@@ -2,7 +2,17 @@ const CACHE_NAME = "simple-pwa-v1";
 const ASSETS = [
     "./",
     "./index.html",
-    "./manifest.webmanifest"
+    "./manifest.webmanifest",
+    "./icons/icon-48x48.png",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-256x256.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
 ];
 
 // Install: pre-cache essential assets
