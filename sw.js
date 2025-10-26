08098c12206f94f8ace3417893491b383e4acfd3
ray-pH <rayhan.azizi9@gmail.com>
2025-10-27 04:06:38 +0700

    init

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "simple-pwa-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.webmanifest",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+// Install: pre-cache essential assets
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+// Activate: clean old caches
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : undefined)))
+        )
+    );
+    self.clients.claim();
+});
+
+// Fetch: cache-first for same-origin requests
+self.addEventListener("fetch", (event) => {
+    const req = event.request;
+    if (new URL(req.url).origin === self.location.origin) {
+        event.respondWith(
+            caches.match(req).then((cached) => cached || fetch(req))
+        );
+    }
+});
